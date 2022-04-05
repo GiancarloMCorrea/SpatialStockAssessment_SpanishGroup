c18e3c7290e25357b140d8919a555d7387c8c2de
Giancarlo H Morón Correa <g.moroncorrea@gmail.com>
2022-04-04 21:06:03 -0700

    #create repo

---
+++ get_initial_files.R
@@ -0,0 +1,92 @@
+
+get_initial_files = function(sim_dat, ctl, dat, selex_len, selex_age, selex_df) {
+
+  # INIT INFO:
+  
+  OutDataFile = dat
+  OutControlFile = ctl
+  myDataFile = sim_dat
+  
+  # DATA FILE :
+  
+  OutDataFile$N_areas = myDataFile$N_areas
+  OutDataFile$Nfleets = myDataFile$Nfleet + myDataFile$Nsurveys
+  OutDataFile$fleetinfo = data.frame(type = c(rep(1, times = myDataFile$Nfleet), rep(3, times = myDataFile$Nsurveys)), 
+                                     surveytiming = c(rep(-1, times = myDataFile$Nfleet), rep(7, times = myDataFile$Nsurveys)),
+                                     area = as.vector(as.matrix(myDataFile$fleetinfo1[2,])),
+                                     units = 2, 
+                                     need_catch_mult = 0,
+                                     fleetname = myDataFile$fleetnames)
+  OutDataFile$fleetnames = myDataFile$fleetnames
+  OutDataFile$surveytiming = c(rep(-1, times = myDataFile$Nfleet), rep(7, times = myDataFile$Nsurveys))
+  OutDataFile$units_of_catch = rep(2, times = OutDataFile$Nfleets)
+  OutDataFile$areas = as.vector(as.matrix(myDataFile$fleetinfo1[2,]))
+  #Catch data:
+  catch_330 = data.frame(year = 1000 + rep(myDataFile$styr:myDataFile$endyr, times = myDataFile$Nfleet), 
+                         seas = 1, 
+                         fleet = rep(1:myDataFile$Nfleet, each = myDataFile$endyr))
+  CatchVec = NULL
+  for(i in 1:myDataFile$Nfleet) {
+    CatchVec = c(CatchVec, myDataFile$catch[,i])
+  }
+  catch_330$catch = CatchVec
+  catch_330$catch_se = rep(myDataFile$se_log_catch, each =  myDataFile$endyr)
+  catch_330 = rbind(data.frame(year = -999, seas = 1, fleet = 1:myDataFile$Nfleet, catch = 0, catch_se = myDataFile$se_log_catch),
+                    catch_330)
+  OutDataFile$catch = catch_330
+  #continue..
+  OutDataFile$CPUEinfo = data.frame(Fleet = myDataFile$CPUEinfo$Fleet, Units = 0, Errtype = myDataFile$CPUEinfo$Errtype,
+                                    SD_Report = 0)
+  rownames(OutDataFile$CPUEinfo) = myDataFile$fleetnames
+  #CPUE data:
+  index_vec = as.numeric(as.character(myDataFile$CPUE$index))
+  if(any(is.na(index_vec))) index_vec = 8 # one area
+  cpue_330 = data.frame(year = 1000 + as.numeric(as.character(myDataFile$CPUE$year)), 
+                        seas = 7, 
+                        index = index_vec,
+                        obs = as.numeric(as.character(myDataFile$CPUE$cpu)), 
+                        se_log = as.numeric(as.character(myDataFile$CPUE$cv)))
+  OutDataFile$CPUE = cpue_330
+  #continue...
+  OutDataFile$len_info = data.frame(mintailcomp = -0.001, addtocomp = myDataFile$add_to_comp, 
+                                    combine_M_F = 0, CompressBins = 0, CompError = 0, ParmSelect = 0,
+                                    minsamplesize = rep(0.01, times = OutDataFile$Nfleets))
+  rownames(OutDataFile$len_info) = OutDataFile$fleetnames
+  OutDataFile$lencomp = myDataFile$lencomp
+  OutDataFile$lencomp$Yr = 1000 + OutDataFile$lencomp$Yr
+  OutDataFile$lencomp$Seas = 7 # should be 7 or 1?
+  OutDataFile$Nfleet = myDataFile$Nfleet
+  OutDataFile$Nsurveys = myDataFile$Nsurveys
+  OutDataFile$fleetinfo1 = t(OutDataFile$fleetinfo[,c('surveytiming', 'area', 'type')])
+  OutDataFile$fleetinfo2 = t(data.frame(units = OutDataFile$units_of_catch, need_catch_mult = 0))
+  OutDataFile$max_combined_lbin = rep(myDataFile$max_combined_lbin, times = OutDataFile$Nfleets)
+  
+  # CONTROL FILE
+  
+  OutControlFile$N_areas = myDataFile$N_areas
+  OutControlFile$Nfleets = myDataFile$Nfleet + myDataFile$Nsurveys
+  OutControlFile$fleetnames = myDataFile$fleetnames
+  OutControlFile$recr_dist_read = myDataFile$N_areas
+  OutControlFile$recr_dist_pattern = data.frame(GPattern = 1, month = 1, area = 1:myDataFile$N_areas, 
+                                                age = 0)
+  OutControlFile$Q_options = data.frame(fleet = (myDataFile$Nfleet+1):(myDataFile$Nfleet+myDataFile$Nsurveys), 
+                                        link = 0, link_info = 0, extra_se = 0, biasadj = 0, float = 0)
+  rownames(OutControlFile$Q_options) = myDataFile$fleetnames[(myDataFile$Nfleet+1):(myDataFile$Nfleet+myDataFile$Nsurveys)]
+  OutControlFile$Q_parms = data.frame(LO = -15, HI = 15, INIT = rep(0, times = myDataFile$Nsurveys), PRIOR = 0, PR_SD = 0,
+                                      PR_type = 0, PHASE = 3, env_var = 0,
+                                      dev_link = 0, dev_minyr = 0, dev_maxyr = 0, dev_PH = 0, Block = 0, Block_Fxn = 0)
+  rownames(OutControlFile$Q_parms) = paste0('LnQ', myDataFile$fleetnames[(myDataFile$Nfleet+1):(myDataFile$Nfleet+myDataFile$Nsurveys)])
+  
+  
+  OutControlFile$size_selex_types = selex_len
+  OutControlFile$age_selex_types = selex_age
+  OutControlFile$size_selex_parms = selex_df
+  OutControlFile$age_selex_parms = NULL
+  
+  # OUT FILES
+
+  myFiles = list(dat = OutDataFile, ctl = OutControlFile)
+  
+  return(myFiles)
+
+}
---
+++ run_models.R
@@ -0,0 +1,82 @@
+# Clean workspace:
+rm(list = ls())
+
+# Set working directory
+mainDir = 'C:/Users/moroncog/Documents/StockAssessmentModels/SpatialStockAssessmentGroup'
+setwd(mainDir)
+
+# Libraries:
+require(r4ss)
+require(Rfssa)
+source('get_initial_files.R')
+
+# -------------------------------------------------------------------------
+# Read data from Github:
+mydata = Rfssa::load_github_data(github_data_url = "https://github.com/aaronmberger-nwfsc/Spatial-Assessment-Modeling-Workshop/blob/main/data/YFT_1area_observations_1_100_ESS_25.RData")
+type_data = '1area_25'
+dir.create(path = type_data)
+
+# -------------------------------------------------------------------------
+# Selectivity parameters:
+selex_len = data.frame(Pattern = c(24,24,1,24,24,24,24,15),
+                             Discard = 0,
+                             Male = 0,
+                             Special = c(0,0,0,0,0,0,0,3))
+selex_age = data.frame(Pattern = rep(0, times = 8),
+                             Discard = 0, Male = 0, Special = 0)
+selex_params_double = data.frame(LO   = c(5,-15,-15,-15,-15,-15),
+                                 HI   = c(130,15,15,15,15,15),
+                                 INIT = c(50, 0, 5,6,-10,-10),
+                                 PRIOR = 0, PR_SD = 0, PR_type = 0, PHASE = c(4,4,4,4,4,4), 
+                                 env_var = 0, dev_link = 0, dev_minyr = 0,
+                                 dev_maxyr = 0, dev_PH = 0, Block = 0, Block_Fxn = 0)
+selex_params_logistic = data.frame(LO = c(5,0),
+                                   HI   = c(130,60),
+                                   INIT = c(50,10),
+                                   PRIOR = 0, PR_SD = 0, PR_type = 0, PHASE = c(4,4), 
+                                   env_var = 0, dev_link = 0, dev_minyr = 0,
+                                   dev_maxyr = 0, dev_PH = 0, Block = 0, Block_Fxn = 0)
+selex_df = rbind(selex_params_double,selex_params_double,selex_params_logistic,
+                        selex_params_double,selex_params_double,selex_params_double,
+                        selex_params_double)
+
+
+# -------------------------------------------------------------------------
+# Read template:
+template_dat = r4ss::SS_readdat_3.30(file = 'base/data_ss.dat')
+template_ctl = r4ss::SS_readctl_3.30(file = 'base/control_ss.ctl', datlist = 'base/data.ss_new')
+template_start = r4ss::SS_readstarter(file = 'base/starter.ss')
+template_fore = r4ss::SS_readforecast(file = 'base/forecast.ss')
+
+
+# -------------------------------------------------------------------------
+# BEGIN LOOP:
+
+for(i in seq_along(mydata)) {
+  
+  dir.create(path = file.path(type_data, mydata[i]))
+  this_data = get(mydata[i])
+  
+  # -------------------------------------------------------------------------
+  # Run models:
+  
+  outFiles = get_initial_files(sim_dat = this_data, ctl = template_ctl, dat = template_dat, 
+                    selex_len = selex_len, selex_age = selex_age, selex_df = selex_df)
+  
+  # -------------------------------------------------------------------------
+  # Write SS files:
+  
+  r4ss::SS_writedat_3.30(datlist = outFiles$dat, outfile = paste0(file.path(type_data, mydata[i]), '/data_ss.dat'), overwrite = TRUE)
+  r4ss::SS_writestarter(mylist = template_start, file = paste0(file.path(type_data, mydata[i]), '/starter.ss'), overwrite = TRUE)
+  r4ss::SS_writeforecast(mylist = template_fore, file = paste0(file.path(type_data, mydata[i]), '/forecast.ss'), overwrite = TRUE)
+  r4ss::SS_writectl_3.30(ctllist = outFiles$ctl, outfile = paste0(file.path(type_data, mydata[i]), '/control_ss.ctl'), overwrite = TRUE)
+  
+  # -------------------------------------------------------------------------
+  # Run SS3:
+  
+  dir = paste0(mainDir, '/', file.path(type_data, mydata[i])) 
+  command = paste("cd", dir, "& ss -nohess", sep = " ")
+  ss = shell(cmd = command, intern = T, wait = T)
+  
+}
+
